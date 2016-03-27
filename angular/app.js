5ce5762cd47fb927a3f19d2be2066e4ef60087d4
Brayan Camargo <brayan440@hotmail.com>
2016-03-27 12:13:10 -0500

    Conexion web server por ajax, devolucion de Json y informacion mostrada con angular

--- angular/app.js
+++ angular/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("appGrability",[]);
+// var app = angular.module("appGrability",[]);
 
 // Conttolador ↓↓
 // app.controller('controlCarga', function ($scope , $http){
@@ -9,16 +9,16 @@ var app = angular.module("appGrability",[]);
 // });
 
 
-app.controller("controlCarga", function ($scope, $http) {
-	$http.get('js/news_mock.json').
-		success(function(respuesta, status, headers, config) {
-			$scope.dataJson = respuesta;
-		}).
-		error(function(data, status, headers, config) {
-	});
+// app.controller("controlCarga", function ($scope, $http) {
+// 	$http.get('js/news_mock.json').
+// 		success(function(respuesta, status, headers, config) {
+// 			$scope.dataJson = respuesta;
+// 		}).
+// 		error(function(data, status, headers, config) {
+// 	});
 
-	// 
-	$scope.menu = function menu ($scope) {
-		$scope.datos.show = false;
-	}
-});
+// 	// 
+// 	// $scope.menu = function menu ($scope) {
+// 	// 	$scope.datos.show = false;
+// 	// }
+// });
--- js/conexion.js
+++ js/conexion.js
@@ -1,14 +1,23 @@
-// $(document).ready(function() {
+var app = angular.module("appGrability",[]);
 
-// 	$.ajax({
-// 		url : '	',
-// 		type: 'GET',
-// 		dataType : 'jsonp',
-// 		jsonp: 'callback',
-// 		success : function(respuesta) {
-// 			console.log(respuesta.mensaje);
-// 			alert(respuesta.mensaje);
-// 		},
-// 	});
+app.controller("controlCarga", function ($scope, $http) {
 
-// });
+	// ajax
+	$.ajax({
+		url : 'http://pruebagrability.comxa.com/recibe.php',
+		type: 'GET',
+		dataType : 'jsonp',
+		jsonp: 'callback',
+		success : function(respuesta) {
+			var respuesta = jQuery.parseJSON( respuesta );
+			$scope.dataJson = respuesta;
+		},
+	});
+	// Ajax Fin
+
+	$scope.custom = true;
+	$scope.toggleCustom = function() {
+		$scope.custom = $scope.custom === false ? true: false;
+	};
+
+});
